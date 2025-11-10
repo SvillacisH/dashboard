6924bf9756ede40e569334ca7586c309bf6f5c6b
Santiago Alejandro Villacis Huerta <savihuer@espol.edu.ec>
2025-11-10 19:08:06 +0000

    Avance Guia12

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import './App.css'
+import { Grid } from '@mui/material';
+
+function App() {
+    return (
+        <Grid>
+
+            {/* Encabezado */}
+            <Grid>Elemento: Encabezado</Grid>
+
+            {/* Alertas */}
+            <Grid>Elemento: Alertas</Grid>
+
+            {/* Selector */}
+            <Grid>Elemento: Selector</Grid>
+
+            {/* Indicadores */}
+            <Grid>Elemento: Indicadores</Grid>
+
+            {/* Gráfico */}
+            <Grid>Elemento: Gráfico</Grid>
+
+            {/* Tabla */}
+            <Grid>Elemento: Tabla</Grid>
+
+            {/* Información adicional */}
+            <Grid>Elemento: Información adicional</Grid>
+
+        </Grid>
+    );
+}
+
+
+export default App;
